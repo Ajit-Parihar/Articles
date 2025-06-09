6d69f554f7a8a4269a6e0f3cbf5b6f4c21c8fe49
Ajit Parihar <pariharajit5@gmail.com>
2025-06-09 18:30:37 +0530

    Revert "I have add another function in user controller"
    This reverts commit 8649c204d2bae14a55fdc6573df2d854b5be00b5.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
-  def for_testing
 
+  def for_testing
+    
   end
 
   def create
       @user = User.new(user_params)
-
-      puts @user.inspect
-
       if @user.save
+        session[:user_id] = @user.id
          send_welcome_email(@user)
         redirect_to articles_path notice: "new user create"
       else
@@ -22,14 +20,12 @@ class UsersController < ApplicationController
   end
 
   def send_welcome_email (user)
-
     UserMailer.welcome_email(user).deliver 
-       
   end
 
   private
-
   def user_params
       params.require(:user).permit(:id, :first_name, :Last_name, :email, :profileImage, :password, :password_confirmation)
   end
+
 end
