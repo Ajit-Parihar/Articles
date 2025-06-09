c8cb9ab0731bb040e246c4dfa9109bfd21e626fd
Ajit Parihar <pariharajit5@gmail.com>
2025-06-09 16:49:21 +0530

    add some changes like puts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,41 +1,36 @@
 class ArticlesController < ApplicationController
   #before_action :set_article, only: [:edit, :update, :show, :destroy]
-  def index
-    # @articles = Article.all
-    # @popular_articles = Article.limit(5)
-     @popular_articles = Rails.cache.fetch('popular_articles', expires_in: 1.hour) do     #  low level caching 
-      Article.limit(5)
- end
-  end
-
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def create 
-
-  puts params[:article]
-  puts current_user[:id].inspect
-    
-    if current_user.articles.create(article_params)  
-         flash[:notice] = "article created"
-         redirect_to ownArticle_path
+      def index
+        # @articles = Article.all
+        # @popular_articles = Article.limit(5)
+        @popular_articles = Rails.cache.fetch('popular_articles', expires_in: 1.hour) do     #  low level caching 
+          Article.limit(5)
     end
-end
+      end
 
-def update
+      def show
+        @article = Article.find(params[:id])
+      end
 
-  @article = Article.find(params[:id])
-  if @article.update(article_params)
-    redirect_to ownArticle_path, notice: "Article updated successfully"
-  else
-    render :edit
-  end
-end
+      def create 
+        current_user = User.find(session[:user_id])
+        if current_user.articles.create(article_params)  
+            flash[:notice] = "article created"
+            redirect_to ownArticle_path
+        end
+    end
 
-private
-def article_params
-  params.require(:article).permit(:title, :body)
-end
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to ownArticle_path, notice: "Article updated successfully"
+      else
+        render :edit
+      end
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,8 @@ class UsersController < ApplicationController
 
   def create
       @user = User.new(user_params)
-
-      puts @user.inspect
-
-
       if @user.save
+        session[:user_id] = @user.id
          send_welcome_email(@user)
         redirect_to articles_path notice: "new user create"
       else
@@ -19,14 +16,12 @@ class UsersController < ApplicationController
   end
 
   def send_welcome_email (user)
-
     UserMailer.welcome_email(user).deliver 
-       
   end
 
   private
-
   def user_params
       params.require(:user).permit(:id, :first_name, :Last_name, :email, :profileImage, :password, :password_confirmation)
   end
+
 end
